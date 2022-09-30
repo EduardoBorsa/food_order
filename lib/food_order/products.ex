ea80d4b8f7a6c0d665e8810bceb7baa8daadd488
Eduardo Borsa <borsa.dado@gmail.com>
2022-09-30 08:43:46 -0300

    chore: update doc to make doctor pass

--- lib/food_order/products.ex
+++ lib/food_order/products.ex
@@ -1,9 +1,20 @@
 defmodule FoodOrder.Products do
+  @moduledoc """
+  Provides a functionality to work with products.
+  """
   alias FoodOrder.Products.Product
   alias FoodOrder.Repo
 
+  @doc """
+    List all product.
+  """
+  @spec list_products() :: list(Product.t())
   def list_products, do: Repo.all(Product)
 
+  @doc """
+    Creates a product.
+  """
+  @spec create_product(map()) :: {:ok, Product.t()} | {:error, Ecto.Changeset.t()}
   def create_product(attrs \\ %{}) do
     attrs
     |> Product.changeset()
